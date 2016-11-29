8d0f34afbd498d8e4bca8d32f4eee7d4fdd903d4
keelanfh <info@keelanfh.com>
2016-11-29 18:26:29 +0000

    username

---
+++ app/src/main/java/com/example/keelan/quizapp/User.java
@@ -0,0 +1,19 @@
+package com.example.keelan.quizapp;
+
+/**
+ * Created by keelan on 29/11/16.
+ */
+
+public class User {
+
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
