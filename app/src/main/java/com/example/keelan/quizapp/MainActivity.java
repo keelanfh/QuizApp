e46ba13a8bab4bdde6bbf7798ee6d29978847a6d
keelanfh <info@keelanfh.com>
2016-11-29 18:40:41 +0000

    setUsername

--- app/src/main/java/com/example/keelan/quizapp/MainActivity.java
+++ app/src/main/java/com/example/keelan/quizapp/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.keelan.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mSubmitButton;
+    private EditText mUsernameBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSubmitButton = (Button) findViewById(R.id.submit_button);
+        mUsernameBox = (EditText) findViewById(R.id.username_box);
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User user = new User();
+                user.setUsername(mUsernameBox.getText().toString());
+
+                Toast.makeText(MainActivity.this,
+                        user.getUsername(),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
