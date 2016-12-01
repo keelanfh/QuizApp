ef41fc809c88c9f8ddf34087c4317833071f4369
keelanfh <info@keelanfh.com>
2016-12-01 09:54:53 +0000

    change code analysis settings

--- app/src/main/java/com/example/keelan/quizapp/App.java
+++ app/src/main/java/com/example/keelan/quizapp/App.java
@@ -4,7 +4,7 @@ package com.example.keelan.quizapp;
  * Created by keelan on 30/11/16.
  */
 
-public class App {
+class App {
 
     static User currentUser = new User();
 
--- app/src/main/java/com/example/keelan/quizapp/User.java
+++ app/src/main/java/com/example/keelan/quizapp/User.java
@@ -4,15 +4,15 @@ package com.example.keelan.quizapp;
  * Created by keelan on 29/11/16.
  */
 
-public class User {
+class User {
 
     private String username;
 
-    public String getUsername() {
+    String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
+    void setUsername(String username) {
         this.username = username;
     }
 
